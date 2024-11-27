38be4a51643b0b487649bb1f4c3431eff736de1f
Qcsteeven <leva.demonov2005@gmail.com>
2024-11-28 02:13:50 +0800

    check optimal 2 ver.

--- transport_task.py
+++ transport_task.py
@@ -16,24 +16,59 @@ class Transport:
         self.method = Fogel()
         self.matrix = self.method.create(self.m, self.a, self.n, self.b, self.matrix)
         self.method.show()
-        self.is_optimal()
+        self.count = 1
+        print(f"{self.count} итерация:")
+        
+        while not self.is_optimal():
+            self.count += 1
+            self.show()
+            print(f"{self.count} итерация:")
+        print("Оптимальный план:")
+        self.show()
 
     def is_optimal(self):
         v = [None for _ in range(self.n)]
         u = [None for _ in range(self.m)]
         u[0] = 0
         basis = self.method.get_basis()
-        for i in range(len(basis)):
-            print(basis[i][0].tariff, basis[i][0].qua, basis[i][0].is_base, basis[i][1])
 
-        while any(v[i] is None for i in range(n)) and any(u[i] is None for i in range(m)):
+        while any(v[i] is None for i in range(self.n)) or any(u[i] is None for i in range(self.m)):
             for x in range(len(basis)):
-                tariff, qua, is_base, i, j = basis[i][0].tariff, basis[i][0].qua, basis[i][0].is_base, basis[i][1]
+                tariff, qua, is_base, i, j = basis[x][0].tariff, basis[x][0].qua, basis[x][0].is_base, *basis[x][1]
                 if (not u[i] is None) and (v[j] is None):
                     v[j] = tariff + u[i]
                 elif (u[i] is None) and (not v[j] is None):
                     u[i] = v[j] - tariff
-            
+                    
+        eval_matrix = [[0 for _ in range(self.n)] for i in range(self.m)]
+        for i in range(self.m):
+            for j in range(self.n):
+                eval_matrix[i][j] = self.matrix[i][j].tariff - (v[j] - u[i])
+        
+        print("Проверка на оптимальность: ")
+        for i in range(self.m):
+            print(f"u{Instr.to_down_index(i + 1)} = {u[i]}")
+        for j in range(self.n):
+            print(f"v{Instr.to_down_index(j + 1)} = {v[j]}")
+        
+        print("Оценочная матрица:")
+        indent = max(max(len(Instr.to_str(j)) for j in i) for i in eval_matrix) + 1
+        for i in range(self.m):
+            out = ""
+            for j in range(self.n):
+                cur = Instr.to_str(eval_matrix[i][j])
+                out += cur + " " * (indent + 1 - len(cur))
+            print(out)
+        
+        fnd_min = float("inf")
+        self.idx_min = (0, 0)
+        for i in range(self.m):
+            for j in range(self.n):
+                if eval_matrix[i][j] < fnd_min:
+                    fnd_min = eval_matrix[i][j]
+                    self.idx_min = (i, j)
+        return fnd_min >= 0
+                 
 
     def build_math_model(self):
         print()
@@ -56,7 +91,7 @@ class Transport:
             print("Σa == Σb")
             print("Фиктивный поставщик/потребитель не требуется")
         print("Получаем начальную таблицу:")
-        self.show()
+        self.show_start()
         print("\nМатематическая модель задачи:")
         out = "L = "
         for i in range(self.m):
@@ -73,7 +108,7 @@ class Transport:
                 if j == self.n - 1:
                     out += f"x{Instr.to_down_index(i + 1)}{Instr.to_down_index(j + 1)}"
                 else:
-                    out += Instr.to_str(self.matrix[i][j]) + f"x{Instr.to_down_index(i + 1)}{Instr.to_down_index(j + 1)} + "
+                    out += f"x{Instr.to_down_index(i + 1)}{Instr.to_down_index(j + 1)} + "
             out += f" = {self.a[i]}"
             print(out)
             
@@ -83,13 +118,13 @@ class Transport:
                 if i == self.m - 1:
                     out += f"x{Instr.to_down_index(i + 1)}{Instr.to_down_index(j + 1)}"
                 else:
-                    out += Instr.to_str(self.matrix[i][j]) + f"x{Instr.to_down_index(i + 1)}{Instr.to_down_index(j + 1)} + "
+                    out += f"x{Instr.to_down_index(i + 1)}{Instr.to_down_index(j + 1)} + "
             out += f" = {self.b[j]}"
             print(out)
         
         
 
-    def show(self):
+    def show_start(self):
         indent = max([max(max([len(Instr.to_str(x)) + 4 for x in self.matrix[i]]) for i in range(self.m)), 9])
         out = "Ai \ Bj" + (indent - 6) * " "
         for j in range(len(self.b)):
@@ -106,3 +141,25 @@ class Transport:
                 out += cur + " " * (indent + 1 - len(cur))
             print(out)
             
+    def show(self):
+        indent = max([max(max([len(Instr.to_str(x.tariff)) + 4 for x in self.matrix[i]]) for i in range(self.m)), 9]) * 2
+        out = indent * " "
+        for j in range(len(self.b)):
+            symbol = f'B{Instr.to_down_index(j + 1)} = {self.b[j]}'
+            out += symbol + " " * (indent + 1 - len(symbol))
+        print(out)
+        
+        for i in range(len(self.a)):
+            out = ""
+            symbol = f'A{Instr.to_down_index(i + 1)} = {self.a[i]}'
+            out += symbol + " " * (indent + 1 - len(symbol))
+            for j in range(len(self.matrix[i])):
+                cur = Instr.to_str(self.matrix[i][j].tariff) + " | "
+                if self.matrix[i][j].is_base:
+                    cur += Instr.to_str(self.matrix[i][j].qua) + " ✔"
+                else:
+                    cur += "0"
+                out += cur + " " * (indent + 1 - len(cur))
+            print(out)
+        
+       
