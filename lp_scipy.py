3c71e614d832328e7c6164550e717c9feb337244
Demonov Lev <leva.demonov2005@gmail.com>
2024-12-14 12:32:40 +0800

    create remove branchs

--- lp_scipy.py
+++ lp_scipy.py
@@ -22,26 +22,37 @@ class LinProg(Prog):
         first_res = linprog(c=self.aim, A_ub=self.table, b_ub=self.free_members, bounds=self.limits, method="highs")
         best_solves = [([first_res.x[i] for i in range(self.sz_aim)], -first_res.fun if self.is_max else first_res.fun)]
         solves = [[self.table, self.free_members]]
+        optimum = None
+        if self.is_max:
+            optimum = float("-inf")
+        else:
+            optimum = float("inf")
         while any(any(not elem.is_integer() for elem in slv[0]) for slv in best_solves):
             not_ints = set()
             new_solves = []
             for i in range(len(best_solves)):
                 cur = best_solves[i][0]
-                for j in range(len(cur)):
-                    if not cur[j].is_integer():
-                        not_ints.add(i)
-                        new_str_left = [1 if k == j else 0 for k in range(self.sz_aim)]
-                        new_str_right = [-1 if k == j else 0 for k in range(self.sz_aim)]
-                        new_solves.append([solves[i][0] + [new_str_left], solves[i][1] + [floor(cur[j])]])
-                        new_solves.append([solves[i][0] + [new_str_right], solves[i][1] + [-ceil(cur[j])]])
+                if (self.is_max and best_solves[i][1] > optimum) or ((not self.is_max) and best_solves[i][1] < optimum):
+                    for j in range(len(cur)):
+                        if not cur[j].is_integer():
+                            not_ints.add(i)
+                            new_str_left = [1 if k == j else 0 for k in range(self.sz_aim)]
+                            new_str_right = [-1 if k == j else 0 for k in range(self.sz_aim)]
+                            new_solves.append([solves[i][0] + [new_str_left], solves[i][1] + [floor(cur[j])]])
+                            new_solves.append([solves[i][0] + [new_str_right], solves[i][1] + [-ceil(cur[j])]])
+                else:
+                    not_ints.add(i)
+                
             best_solves = [best_solves[i] for i in range(len(best_solves)) if i not in not_ints]
             solves = [solves[i] for i in range(len(solves)) if i not in not_ints]
             for i in range(len(new_solves)):
                 res = linprog(c=self.aim, A_ub=new_solves[i][0], b_ub=new_solves[i][1], bounds=self.limits, method="highs")
                 if res.success:
                     cur = ([res.x[i] for i in range(self.sz_aim)], -res.fun if self.is_max else res.fun)
-                    solves.append(new_solves[i])
-                    best_solves.append(cur)
+                    if (self.is_max and cur[1] > optimum) or (self.is_max and cur[1] < optimum):
+                        solves.append(new_solves[i])
+                        best_solves.append(cur)
+        
         answer = max(best_solves, key = lambda x : x[1])
         print(f"F = {answer[1]}") 
         for i in range(len(answer[0])):
